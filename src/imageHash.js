54460d3f68699ab4b4424536d6354ea19f6d85dd
test <you@example.com>
2020-04-04 03:19:09 +0800

    Remove console.warn

--- src/imageHash.js
+++ src/imageHash.js
@@ -50,7 +50,6 @@ module.exports = (oldSrc, bits, method, cb) => {
         cb(new Error(`Unrecognized file extension, mime type or mismatch, ext: ${ext} / mime: ${type.mime}`));
       }
     } else {
-      console.warn('No file extension found, attempting mime typing.');
       if (type.mime === 'image/png') {
         processPNG(data, bits, method, cb);
       } else if (type.mime === 'image/jpeg') {
